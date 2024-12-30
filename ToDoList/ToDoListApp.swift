1d9a033c233e82b74706d8ebc6786dfb70dc0681
Sethvin Nanayakkara <nanasethvin@gmail.com>
2024-12-29 21:28:31 -0600

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Sethvin Nanayakkara on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
